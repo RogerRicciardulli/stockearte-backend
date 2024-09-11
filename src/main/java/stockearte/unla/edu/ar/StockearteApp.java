ed4902042461d8b73a472fb18b3d525be3d61708
Roger <rricciardulli@certant.com>
2024-09-11 16:04:30 -0300

    Quitando archivos innecesarios

--- src/main/java/stockearte/unla/edu/ar/StockearteApp.java
+++ src/main/java/stockearte/unla/edu/ar/StockearteApp.java
@@ -6,11 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
-@EnableSwagger2
-@ComponentScan(basePackages = { "stockearte.unla.edu.ar.*", "io.swagger.configuration"})
+@ComponentScan(basePackages = { "stockearte.unla.edu.ar.*"})
 public class StockearteApp implements CommandLineRunner {
 
     @Override
